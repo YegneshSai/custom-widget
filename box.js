f83e84c6faf9a54fc7cff49ef9a3fe89dd03ed8b
YegneshSai <103030764+YegneshSai@users.noreply.github.com>
2023-11-08 11:14:10 +0530

    Update box.js

--- box.js
+++ box.js
@@ -6,7 +6,7 @@
 			border-radius: 2px;
    			box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
       			border-style:solid;
-      			border-color:"#0F52BA";
+      			border-color:"#019CEO";
 			display: block;
 		} 
 		</style> 
